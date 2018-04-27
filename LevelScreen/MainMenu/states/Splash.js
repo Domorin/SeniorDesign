ed28977bc617b1a9a13f9cbbf121bd9ed05e57ad
Cody Lutzel <domorin821@gmail.com>
2018-04-26 21:59:53 -0400

    fixed menu

--- LevelScreen/MainMenu/states/Splash.js
+++ LevelScreen/MainMenu/states/Splash.js
@@ -3,33 +3,33 @@ var Splash = function () {};
 Splash.prototype = {
 
   loadScripts: function () {
-    game.load.script('style', 'lib/style.js');
-    game.load.script('mixins', 'lib/mixins.js');
-    game.load.script('WebFont', 'vendor/webfontloader.js');
-    game.load.script('gamemenu','states/GameMenu.js');
-    game.load.script('game', 'states/Game.js');
-    game.load.script('gameover','states/GameOver.js');
-    game.load.script('credits', 'states/Credits.js');
-    game.load.script('options', 'states/Options.js');
+    game.load.script('style', 'MainMenu/lib/style.js');
+    game.load.script('mixins', 'MainMenu/lib/mixins.js');
+    game.load.script('WebFont', 'MainMenu/vendor/webfontloader.js');
+    game.load.script('gamemenu','MainMenu/states/GameMenu.js');
+    game.load.script('game', 'MainMenu/states/Game.js');
+    game.load.script('gameover','MainMenu/states/GameOver.js');
+    game.load.script('credits', 'MainMenu/states/Credits.js');
+    game.load.script('options', 'MainMenu/states/Options.js');
   },
 
   loadBgm: function () {
     // thanks Kevin Macleod at http://incompetech.com/
-    game.load.audio('dangerous', 'assets/bgm/Dangerous.mp3');
-    game.load.audio('exit', 'assets/bgm/Exit the Premises.mp3');
+    game.load.audio('dangerous', 'MainMenu/assets/bgm/Dangerous.mp3');
+    game.load.audio('exit', 'MainMenu/assets/bgm/Exit the Premises.mp3');
   },
   // varios freebies found from google image search
   loadImages: function () {
-    game.load.image('menu-bg', 'assets/images/menu-bg2.png');
-    game.load.image('options-bg', 'assets/images/options1024x768.png');
-    game.load.image('gameover-bg', 'assets/images/gameover1024x768png.png');
+    game.load.image('menu-bg', 'MainMenu/assets/images/menu-bg2.png');
+    game.load.image('options-bg', 'MainMenu/assets/images/options1024x768.png');
+    game.load.image('gameover-bg', 'MainMenu/assets/images/gameover1024x768png.png');
   },
 
   loadFonts: function () {
     WebFontConfig = {
       custom: {
         families: ['TheMinion'],
-        urls: ['assets/style/theminion.css']
+        urls: ['MainMenu/assets/style/theminion.css']
       }
     }
   },
@@ -42,7 +42,7 @@ Splash.prototype = {
   },
 
   preload: function () {
-    game.add.sprite(0, 0, 'grass1024x768');
+    game.add.sprite(0, 0, 'grass');
     game.add.existing(this.logo).scale.setTo(0.5);
     game.add.existing(this.loadingBar);
     game.add.existing(this.status);
@@ -58,7 +58,7 @@ Splash.prototype = {
   addGameStates: function () {
 
     game.state.add("GameMenu",GameMenu);
-    game.state.add("Game",Game);
+    game.state.add("levsel",LevelSelect);
     game.state.add("GameOver",GameOver);
     game.state.add("Credits",Credits);
     game.state.add("Options",Options);
--- LevelScreen/MainMenu/states/gamemenu.js
+++ LevelScreen/MainMenu/states/gamemenu.js
@@ -1,4 +1,12 @@
-var GameMenu = function() {};
+(
+    function() {
+
+var game;
+GameMenu = function(currentGame) {
+  console.log("IN GAME MENU!");
+  this.game = game;
+  game = currentGame;
+};
 
 
 GameMenu.prototype = {
@@ -9,6 +17,11 @@ GameMenu.prototype = {
   },
 
   init: function () {
+    game.world.setBounds(0, 0);
+    game.camera.setPosition(0, 0);
+    game.world.width = 1024;
+    game.world.height = 768;
+
     this.titleText = game.make.text(game.world.centerX, 100, "4-H Superheroes", {
       font: 'bold 60pt TheMinion',
       fill: '#FDFFB5',
@@ -17,9 +30,17 @@ GameMenu.prototype = {
     this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
     this.titleText.anchor.set(0.5);
     this.optionCount = 1;
+
   },
 
   create: function () {
+    console.log("HELLO?");
+
+    game.world.setBounds(0, 0);
+    game.camera.setPosition(0, 0);
+
+    game.world.width = 1024;
+    game.world.height = 768;
 
     if (music.name !== "dangerous" && playMusic) {
       music.stop();
@@ -28,11 +49,12 @@ GameMenu.prototype = {
       music.play();
     }
     game.stage.disableVisibilityChange = true;
-    game.add.sprite(0, 0, 'menu-bg');
+    var sprite = game.add.sprite(0, 0, 'menu-bg');
+    sprite.fixedToCamera = true;
     game.add.existing(this.titleText);
 
     this.addMenuOption('Start', function () {
-      game.state.start("Game");
+      game.state.start("levsel");
     });
     this.addMenuOption('Options', function () {
       game.state.start("Options");
@@ -40,7 +62,59 @@ GameMenu.prototype = {
     this.addMenuOption('Credits', function () {
       game.state.start("Credits");
     });
+
+
+  },
+
+  addMenuOption: function(text, callback, className) {
+
+    // use the className argument, or fallback to menuConfig, but
+    // if menuConfig isn't set, just use "default"
+    className || (className = this.menuConfig.className || 'default');
+
+    // set the x coordinate to game.world.center if we use "center"
+    // otherwise set it to menuConfig.startX
+    var x = this.menuConfig.startX === "center" ?
+      game.world.centerX :
+      this.menuConfig.startX;
+
+
+    // set Y coordinate based on menuconfig
+    var y = this.menuConfig.startY;
+
+    console.log(y);
+
+    // create
+    var txt = game.add.text(
+      x,
+      (this.optionCount * 80) + y,
+      text,
+      style.navitem[className]
+    );
+
+    txt.fixedToCamera = true;
+
+    // use the anchor method to center if startX set to center.
+    txt.anchor.setTo(this.menuConfig.startX === "center" ? 0.5 : 0.0);
+
+    txt.inputEnabled = true;
+
+    txt.events.onInputUp.add(callback);
+    txt.events.onInputOver.add(function (target) {
+      target.setStyle(style.navitem.hover);
+    });
+    txt.events.onInputOut.add(function (target) {
+      target.setStyle(style.navitem[className]);
+    });
+
+    this.optionCount ++;
   }
+
 };
 
-Phaser.Utils.mixinPrototype(GameMenu.prototype, mixins);
+//Phaser.Utils.mixinPrototype(GameMenu.prototype, mixins);
+
+
+
+
+}());
