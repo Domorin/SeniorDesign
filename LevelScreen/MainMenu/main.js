c1cc743b39cc5c730c858bb8b6d7f72c2fc5a5f7
Cody Lutzel <domorin821@gmail.com>
2018-04-26 23:39:32 -0400

    fixed logo in splash screen

--- LevelScreen/MainMenu/main.js
+++ LevelScreen/MainMenu/main.js
@@ -17,7 +17,7 @@ Main.prototype = {
   preload: function () {
     game.load.image('grass',    'MainMenu/assets/images/grass1024x768.png');
     game.load.image('loading',  'MainMenu/assets/images/loading.png');
-    game.load.image('brand',    'MainMenu/assets/images/logo.jpg');
+    game.load.image('brand',    'MainMenu/assets/images/logo.png');
     game.load.script('polyfill',   'MainMenu/lib/polyfill.js');
     game.load.script('utils',   'MainMenu/lib/utils.js');
     game.load.script('splash',  'MainMenu/states/Splash.js');
