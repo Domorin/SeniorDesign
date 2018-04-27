10713fbe9304f3c2592b41085b3793fb72f179e3
Cody Lutzel <domorin821@gmail.com>
2018-04-26 22:06:35 -0400

    added event 4 to level screen

--- LevelScreen/Event4/event4.js
+++ LevelScreen/Event4/event4.js
@@ -23,144 +23,322 @@
 
 	function preload() {
 
-	    game.load.image('background','image/lvl4background.png');
-	    game.load.image('player','image/phaser-dude.png');
-		game.load.image('mushroom', 'image/sprites/maggot.png');
-		game.load.spritesheet('veggies', 'image/sprites/fruitnveg32wh37.png', 32, 32);
-        game.load.image("menu", "content/menu.png", 341, 128)
-	}
+    game.load.image('background','Event4/image/lvl4background1024x768.png');
+    game.load.image('player','Event4/image/phaser-dude.png');
+	game.load.image('mushroom', 'Event4/image/sprites/maggot.png');
+	game.load.spritesheet('ball', 'Event4/image/sprites/interference_ball_48x48.png', 48 ,48);
+	game.load.spritesheet('coin', 'Event4/image/sprites/coin.png', 32 ,32);
+	game.load.image('block', 'Event4/image/sprites/block.png', 5 ,5);
+	game.load.image('arrow', 'Event4/image/sprites/arrow.png',5,5);
+}
 
-	var player;
-	var cursors;
-	var angle = 0;
-	var group;
+var player;
+var cursors;
+var angle = 0;
+var group;
+var test;
+var block;
+function create() {
+	
+	//setbounds
+	game.world.setBounds(0,0,1024,768);
 
-	function create() {
+    //add background
+	game.add.tileSprite(0, 0, 1024, 768, 'background');
 
-	    game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    //start physics
+	game.physics.startSystem(Phaser.Physics.P2JS);
+	
+	//add player
+    player = game.add.sprite(300, 600, 'player');
 
-	    game.world.setBounds(0, 0, 1024, 768);
-
-	    game.physics.startSystem(Phaser.Physics.P2JS);
-
-	    player = game.add.sprite(300, 500, 'player');
-
-	    game.physics.arcade.enable(player);
+	//enable player physics
+    game.physics.arcade.enable(player);
+	
+	//mouse pointer functionality
+	game.input.onDown.add(moveBall, this);
+	
+	//add timer
+	game.time.events.add(Phaser.Timer.SECOND * 30.00, quit, this);
+	
+	player.body.collideWorldBounds = true;
+	player.body.bounce.setTo(0.9,0.9);
+	
+	player.body.mass = 100;
+	game.input.addPointer();
+	
+	//enable AI
+	test = game.add.group();
+	test.enableBody = true;
+	test.physicsBodyType = Phaser.Physics.ARCADE;
+	tests = test.create(300,750,'mushroom');
+	tests.body.collideWorldBounds = true;
+	tests.body.bounce.setTo(0.9,0.9);
+	game.physics.arcade.moveToXY(tests,800,630,100);
+	
+	//add obstacles 
+	group = game.add.physicsGroup();
+	group1 = game.add.physicsGroup();
+	//nondestructible
+	    for (var i = 0; i < 10; i++)
+	    {
+	        var c = group.create(game.rnd.between(200, 250), game.rnd.between(510, 530), 'ball',15);
+	        c.body.immovable= true;
+	    }
+	    for (var i = 0; i < 10; i++)
+	    {
+	        var c = group.create(game.rnd.between(750, 755), game.rnd.between(515, 520), 'ball',15);
+	        c.body.immovable= true;
+	    }
+	    for (var i = 0; i < 10; i++)
+	    {
+	        var c = group.create(game.rnd.between(200, 250), game.rnd.between(190, 200), 'ball',15);
+	        c.body.immovable= true;
+	    }
+	    for (var i = 0; i < 10; i++)
+	    {
+	        var c = group.create(game.rnd.between(750, 755), game.rnd.between(190,200), 'ball',15);
+	        c.body.immovable= true;
+	    }
 		
-	    game.camera.follow(player);
-		game.input.onDown.add(moveBall, this);
+	//bottom left corner
+		for (var i =0 ; i< 3; i++)
+		{
+			var c = group1.create(game.rnd.between(0,50),game.rnd.between(690,695),'block', 1);
+			c.body.immovable = true;
+		}
+		//bottom right corner
+		for (var i =0 ; i< 3; i++)
+		{
+			var c = group1.create(game.rnd.between(900,950),game.rnd.between(660,670),'block', 1);
+			c.body.immovable = true;
+		}
+		//bottom right corner
+		for (var i =0 ; i< 3; i++)
+		{
+			var c = group1.create(game.rnd.between(970,975),game.rnd.between(575,576),'block', 1);
+			c.body.immovable = true;
+		}
+		//top right corner
+		for (var i =0 ; i< 5; i++)
+		{
+			var c = group1.create(game.rnd.between(980,985),game.rnd.between(70,90),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 5; i++)
+		{
+			var c = group1.create(game.rnd.between(900,950),game.rnd.between(10,30),'block', 1);
+			c.body.immovable = true;
+		}
+		//left right corner
+		for (var i =0 ; i< 5; i++)
+		{
+			var c = group1.create(game.rnd.between(0,50),game.rnd.between(20,30),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 5; i++)
+		{
+			var c = group1.create(game.rnd.between(0,50),game.rnd.between(10,30),'block', 1);
+			c.body.immovable = true;
+		}
+		//bottom row
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(300,301),game.rnd.between(500,501),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(390,391),game.rnd.between(500,501),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(480,481),game.rnd.between(500,501),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(570,571),game.rnd.between(500,501),'block', 1);
+			c.body.immovable = true;
+		}
+		{
+			var c = group1.create(game.rnd.between(660,661),game.rnd.between(500,501),'block', 1);
+			c.body.immovable = true;
+		}
+		// rightside 
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(739,740),game.rnd.between(330,331),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(738,739),game.rnd.between(420,421),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(740,741),game.rnd.between(240,241),'block', 1);
+			c.body.immovable = true;
+		}
+		//top row
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(300,301),game.rnd.between(160,161),'block', 1);
+			c.body.immovable = true;
+		}
+		{
+			var c = group1.create(game.rnd.between(390,391),game.rnd.between(160,161),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(480,481),game.rnd.between(160,161),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(570,571),game.rnd.between(160,161),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(660,661),game.rnd.between(160,161),'block', 1);
+			c.body.immovable = true;
+		}
+		//left side
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(210,211),game.rnd.between(240,241),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(210,211),game.rnd.between(420,421),'block', 1);
+			c.body.immovable = true;
+		}
+		for (var i =0 ; i< 1; i++)
+		{
+			var c = group1.create(game.rnd.between(210,211),game.rnd.between(330,331),'block', 1);
+			c.body.immovable = true;
+		}
 		
-		game.time.events.add(Phaser.Timer.SECOND * 30.00, quit, this);
-		
-		test = game.add.sprite(300, 500, 'mushroom');
-		game.physics.arcade.enable(test);
-		
-		group = game.add.physicsGroup();
+}
 
-		    for (var i = 0; i < 10; i++)
-		    {
-		        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', game.rnd.between(0, 35));
-		        c.body.mass = -100;
-		    }
+function update() {
 
-		    for (var i = 0; i < 10; i++)
-		    {
-		        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', 17);
-		    }
-
-        AddPauseMenu(game);
-
-	}
-
-	function onDragStart(sprite, pointer) {
-
-	    result = "Dragging " + sprite.key;
-
-	}
-
-	function update() {
-
-	  
-	  if (game.physics.arcade.collide(player, group, collisionHandler, processHandler, this))
-	     {
-	         console.log('boom');
-	     }
+  
+  if (game.physics.arcade.collide(player, group, collisionHandler, processHandler, this))
+     {
+         console.log('boom');
 		 
-	  if (game.physics.arcade.collide(test, group, collisionHandler1, processHandler1,this))
-	        {
-	            console.log('boom');
-	        }
+     }
+	 
+	 
+ if (game.physics.arcade.collide(test, group, collisionHandler1, processHandler1,this))
+        {
+            console.log('boom');
+        } 
 		
-	  if (test.y > 400){
-	  	test.x += 0.3;
-	  	test.y -= 0.065;
-	  }
-	  else if (300< test.y <= 400){
-	  	test.y -= 0.3;
-	  }
-	  else {
-		  test.x += 10;
-		  test.y += 10;
-	  }
-	}
+if (game.physics.arcade.collide(player, group1, collisionHandler3, processHandler3, this))
+	       {
+	           console.log('boom');
+		 
+	       }
+	 
+	 
+ if (game.physics.arcade.collide(test, group1, collisionHandler4, processHandler4,this))
+	       {
+	           console.log('boom');
+	       }
+  
+}
 
 
-	function moveBall() {
+function moveBall() {
 
-	    //  If we don't it'll look very wrong
-	    game.camera.follow();
+    //  If we don't it'll look very wrong
+   // game.camera.follow();
 
-	    game.physics.arcade.moveToPointer(player, 75);
+    game.physics.arcade.moveToPointer(player, 100, game.input.activePointer);
 
-	    //  The maxTime parameter lets you control how fast it will arrive at the Pointer coords
-	    // game.physics.arcade.moveToPointer(ball, 100, game.input.activePointer, 1000);
+    //  The maxTime parameter lets you control how fast it will arrive at the Pointer coords
+    // game.physics.arcade.moveToPointer(ball, 100, game.input.activePointer, 1000);
 
 
-	}
+}
 
-	function processHandler (player, veg) {
+function processHandler (player, veg) {
+
+    return true;
+
+}
+
+function collisionHandler (player, veg) {
+
+    if (veg.frame == 0)
+    {
+        veg.kill();
+    }
+}
+function processHandler1 (tests, veg) {
 
 	    return true;
 
 	}
 
-	function collisionHandler (player, veg) {
+function collisionHandler1 (tests, veg) {
 
-	    if (veg.frame == 17)
+	    if (veg.frame == 0)
 	    {
 	        veg.kill();
 	    }
-	}
-	function processHandler1 (test, veg) {
+}
 
-		    return true;
+function processHandler3 (player, veg) {
 
-		}
+    return true;
 
-	function collisionHandler1 (test, veg) {
+}
 
-		    if (veg.frame == 18)
-		    {
-		        veg.kill();
-		    }
-	}
-	function quit () {
-		
-		if (game.time.events == 0) {
-			stateText.text=" GAME OVER \n Click to restart";
-			        stateText.visible = true;
+function collisionHandler3 (player, veg) {
 
-			        //the "click to restart" handler
-			        game.input.onTap.addOnce(restart,this);
-		}
-	}
+    if (veg.frame == 0)
+    {
+		veg.body.immovable = true;
+    }
+}
+function processHandler4 (tests, veg) {
 
-	function render() {
-
-	    game.debug.cameraInfo(game.camera, 32, 32);
-	    game.debug.spriteCoords(player, 32, 500);
-		game.debug.text("Time until event: " + game.time.events.duration, 200, 100);
+	    return true;
 
 	}
 
+function collisionHandler4 (tests, veg) {
+
+	    if (veg.frame == 0)
+	    {
+	       veg.body.immovable = true;
+	    }
+}
+
+function quit () {
+	
+	if (game.time.events == 0) {
+		stateText.text=" GAME OVER \n Click to restart";
+		        stateText.visible = true;
+
+		        //the "click to restart" handler
+		        game.input.onTap.addOnce(restart,this);
+	}
+}
+
+function render() {
+
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player, 0, 680);
+	game.debug.text("Time until event: " + game.time.events.duration, 0, 728);
+
+}
 }());
